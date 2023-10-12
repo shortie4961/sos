8f246c25026ec25a1cbaf2729cb25e8f3cd7181d
shortie4961 <124915924+shortie4961@users.noreply.github.com>
2023-10-12 17:18:50 +0530

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+#1. First Setup Your Basic Information 
+#2. after Try 
+# tutorial > https://termuxxplorer.blogspot.com/2023/06/socpubliccom-termux-script-earn-money.html
+
+
+$user = "xxx;
+
+$cookie = "xxxx";
